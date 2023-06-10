d852759ef4e835973c3a6c675d5b61cc5d508e7f
myismamingyang <126977818+myismamingyang@users.noreply.github.com>
2023-06-10 23:58:37 +0800

    jdbc

--- src/main/scala/spark/sougou/jdbc/mysql/insert.scala
+++ src/main/scala/spark/sougou/jdbc/mysql/insert.scala
@@ -1,6 +1,6 @@
 package spark.sougou.jdbc.mysql
 
-import org.apache.spark.sql.SparkSession
+import java.sql.{Connection, DriverManager, Statement}
 
 /**
  * @Author: Mingyang Ma
@@ -10,36 +10,46 @@ import org.apache.spark.sql.SparkSession
  */
 object insert {
   def main(args: Array[String]): Unit = {
-    val spark = SparkSession.builder().appName("MysqlInsertTest").master("local").getOrCreate()
-    val jdbcDF = spark.read
-      .format("jdbc")
-      .option("url", "jdbc:mysql://node3:3306/SouGouQueryAnalysis?useUnicode=true&characterEncoding=utf-8")
-      .option("driver", "com.mysql.jdbc.Driver") //驱动
-      .option("user", "root")
-      .option("password", "MMYqq123")
-      //.option("dbtable", s"(select COUNT(1) from customer) customer") 直接查询sql数据
-      .option("dbtable", "inserCreat_test") //加载表  // dbtable: 指定查询功能
-      // s"(sql) dbtable"  为查询sql语法
-      .load()
-    var id: Int = 3
-    var tt: String = "sql-3"
-    spark.sql(s"insert into inserCreat_test(id,commit_log) values('${id}','${tt}');")
+
+
+
+    var ip: String = "node3"
+    var database: String = "SouGouQueryAnalysis"
+    var user: String = "root"
+    var cipher: String = "MMYqq123"
+    var tablename: String = "scala_JDBC_test"
+    var columns: String = "commit_log,commit_time"
+    var data: String = "'sql-1','2023-06-10 23:56:30'"
+
+    // insert into scala_JDBC_test(commit_log,commit_time) values("sql-1",date_format(now(),'%Y-%c-%d %h:%i:%s' ));
+    // insert into scala_JDBC_test(commit_log,commit_time) values("sql-1","2023-06-10 23:56:30");
+    insert(ip, database, user, cipher, tablename: String, columns, data)
   }
 
-  def newTables(table: String): Unit = {
-    val spark = SparkSession.builder().appName("MysqlQueryDemo").master("local").getOrCreate()
-    val jdbcDF = spark.read
-      .format("jdbc")
-      .option("url", "jdbc:mysql://node3:3306/testLibrary?useUnicode=true&characterEncoding=utf-8")
-      .option("driver", "com.mysql.jdbc.Driver") //驱动
-      .option("user", "root")
-      .option("password", "MMYqq123")
-      //.option("dbtable", s"(select COUNT(1) from customer) customer") 直接查询sql数据
-      .option("dbtable", table) //加载表  // dbtable: 指定查询功能
-      // s"(sql) dbtable"  为查询sql语法
-      .load()
-    var id: Int = 3
-    var tt: String = "sql-3"
-    spark.sql(s"insert into inserCreat_test(id,commit_log) values('${id}','${tt}');")
+  def insert(ip: String, database: String, user: String, cipher: String, tablename: String, columns: String, data: String): Unit = {
+    val driver = "com.mysql.jdbc.Driver"
+    //val url = "jdbc:mysql://node3:3306/SouGouQueryAnalysis"
+    var url = "jdbc:mysql://" + ip + ":3306/" + database
+    var username = user
+    var password = cipher
+
+
+    var connection: Connection = null
+
+    Class.forName(driver)
+    connection = DriverManager.getConnection(url, username, password)
+    val ps: Statement = connection.createStatement()
+    //val query = s"insert into stu (id, score) values($id , $score)"
+    //ps.addBatch(query)
+    var idint = 7
+    var commit_logvarchar = "log-7"
+
+    println("columns: " + columns)
+    println("data: " + data)
+    var sqlstr: String = s"insert into $tablename ($columns) values($data)"
+    println("sqlstr: " + sqlstr)
+    ps.executeUpdate(sqlstr)
+    ps.close()
+    connection.close()
   }
 }
--- src/main/scala/spark/sougou/jdbc/mysql/jdbc_mysql_OK.scala
+++ src/main/scala/spark/sougou/jdbc/mysql/jdbc_mysql_OK.scala
@@ -36,10 +36,46 @@ object jdbc_mysql_OK {
 
 
     // connect to the database named "mysql" on the localhost
+
+
+    //    val driver = "com.mysql.jdbc.Driver"
+    //    val url = "jdbc:mysql://node3:3306/SouGouQueryAnalysis"
+    //    val username = "root"
+    //    val password = "MMYqq123"
+    //
+    //
+    //    var connection: Connection = null
+    //
+    //    Class.forName(driver)
+    //    connection = DriverManager.getConnection(url, username, password)
+    //    val ps: Statement = connection.createStatement()
+    //    //val query = s"insert into stu (id, score) values($id , $score)"
+    //    //ps.addBatch(query)
+    //    var idint = 7
+    //    var commit_logvarchar = "log-7"
+    //    var sqlstr: String = s"insert into inserCreat_test (id, commit_log) values($idint,'$commit_logvarchar')"
+    //    ps.executeUpdate(sqlstr)
+    //    ps.close()
+    //    connection.close()
+    var ip: String = "node3"
+    var database: String = "SouGouQueryAnalysis"
+    var user: String = "root"
+    var cipher: String = "MMYqq123"
+    var tablename: String = "scala_JDBC_test"
+    var columns: String = "commit_log,commit_time"
+    var data: String = "'sql-1','2023-06-10 23:56:30'"
+
+    // insert into scala_JDBC_test(commit_log,commit_time) values("sql-1",date_format(now(),'%Y-%c-%d %h:%i:%s' ));
+    // insert into scala_JDBC_test(commit_log,commit_time) values("sql-1","2023-06-10 23:56:30");
+    insert(ip, database, user, cipher, tablename: String, columns, data)
+  }
+
+  def insert(ip: String, database: String, user: String, cipher: String, tablename: String, columns: String, data: String): Unit = {
     val driver = "com.mysql.jdbc.Driver"
-    val url = "jdbc:mysql://node3:3306/SouGouQueryAnalysis"
-    val username = "root"
-    val password = "MMYqq123"
+    //val url = "jdbc:mysql://node3:3306/SouGouQueryAnalysis"
+    var url = "jdbc:mysql://" + ip + ":3306/" + database
+    var username = user
+    var password = cipher
 
 
     var connection: Connection = null
@@ -51,12 +87,13 @@ object jdbc_mysql_OK {
     //ps.addBatch(query)
     var idint = 7
     var commit_logvarchar = "log-7"
-    var sqlstr:String = s"insert into inserCreat_test (id, commit_log) values($idint,'$commit_logvarchar')"
+
+    println("columns: " + columns)
+    println("data: " + data)
+    var sqlstr: String = s"insert into $tablename ($columns) values($data)"
+    println("sqlstr: " + sqlstr)
     ps.executeUpdate(sqlstr)
     ps.close()
     connection.close()
-  }
-  def insert(): Unit ={
-
   }
 }
