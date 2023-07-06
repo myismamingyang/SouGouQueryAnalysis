a41c95f0eb4ba3053d73300e99c525778496758e
myismamingyang <126977818+myismamingyang@users.noreply.github.com>
2023-07-06 15:39:08 +0800

    wordJDBC

--- src/main/scala/spark/wordJDBC.scala
+++ src/main/scala/spark/wordJDBC.scala
@@ -11,35 +11,72 @@ import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
  * @Function: spark 读取txt数据文件 生成schema 后将数据insert到 mysql
  */
 object wordJDBC {
-  case class inserttest(id: Int, create_date_time: String, session_id: String)
+  case class windowsMYSQL(
+                           id: Int
+                           , create_date_time: String
+                           , session_id: String
+                         )
+
+  case class linuxMYSQL(
+                         id: Int
+                         , commit_log: String
+                         , commit_time: String
+                       )
 
   def main(args: Array[String]): Unit = {
     val spark: SparkSession = SparkSession.builder().appName("sparksql").master("local[*]").getOrCreate()
     val sc: SparkContext = spark.sparkContext
     sc.setLogLevel("WARN")
 
-    //TODO 1.加载数据
     val lines: RDD[String] = sc.textFile("data/datafile.txt")
-    val personRDD: RDD[inserttest] = lines.map(line => {
+
+//     //windowsMYSQL
+//    val windowsSQLRDD: RDD[windowsMYSQL] = lines.map(line => {
+//      val arr: Array[String] = line.split(" ")
+//      windowsMYSQL(
+//        arr(0).toInt
+//        , arr(1)
+//        , arr(2))
+//    })
+//    import spark.implicits._
+//    val windowsMYSQLRDDDF: DataFrame = windowsSQLRDD.toDF()
+//
+//    windowsMYSQLRDDDF.printSchema()
+//
+//    val windowsMYSQLup = new java.util.Properties()
+//    windowsMYSQLup.put("user", "root")
+//    windowsMYSQLup.put("password", "root")
+//
+//    windowsMYSQLRDDDF.write
+//      .mode(SaveMode.Append)
+//      .jdbc("jdbc:mysql://localhost:3306/test", "inserttest", windowsMYSQLup) //windows MYSQL
+//
+//    println("已完成sql插入 windowsMYSQL")
+//    windowsMYSQLRDDDF.show()
+
+    // linuxMYSQL
+    val linuxSQLRDD: RDD[linuxMYSQL] = lines.map(line => {
       val arr: Array[String] = line.split(" ")
-      inserttest(
+      linuxMYSQL(
         arr(0).toInt
         , arr(1)
         , arr(2))
     })
     import spark.implicits._
-    val personDF: DataFrame = personRDD.toDF()
+    val linuxMYSQLRDDDF: DataFrame = linuxSQLRDD.toDF()
 
-    personDF.printSchema()
-    personDF.show()
+    linuxMYSQLRDDDF.printSchema()
 
-    val connectionProperties = new java.util.Properties()
-    connectionProperties.put("user", "root")
-    connectionProperties.put("password", "root")
 
-    personDF.write
+    val linuxMYSQLup = new java.util.Properties()
+    linuxMYSQLup.put("user", "root")
+    linuxMYSQLup.put("password", "MMYqq123")
+
+    linuxMYSQLRDDDF.write
       .mode(SaveMode.Append)
-      .jdbc("jdbc:mysql://localhost:3306/test", "inserttest", connectionProperties)
+      .jdbc("jdbc:mysql://node3:3306/SouGouQueryAnalysis", "scala_JDBC_test", linuxMYSQLup) //windows MYSQL
+    println("已完成sql插入 linuxMYSQL")
+    linuxMYSQLRDDDF.show()
 
 
     spark.stop()
