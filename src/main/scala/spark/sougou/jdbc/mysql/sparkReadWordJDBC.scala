9d846d37e749385b8c8abc7403236ed99bdde95b
myismamingyang <126977818+myismamingyang@users.noreply.github.com>
2023-07-06 15:58:47 +0800

    sparkReadWordJDBC

--- src/main/scala/spark/sougou/jdbc/mysql/sparkReadWordJDBC.scala
+++ src/main/scala/spark/sougou/jdbc/mysql/sparkReadWordJDBC.scala
@@ -31,53 +31,53 @@ object sparkReadWordJDBC {
 
     val lines: RDD[String] = sc.textFile("data/datafile.txt")
 
-    //     //windowsMYSQL
-    //    val windowsSQLRDD: RDD[windowsMYSQL] = lines.map(line => {
-    //      val arr: Array[String] = line.split(" ")
-    //      windowsMYSQL(
-    //        arr(0).toInt
-    //        , arr(1)
-    //        , arr(2))
-    //    })
-    //    import spark.implicits._
-    //    val windowsMYSQLRDDDF: DataFrame = windowsSQLRDD.toDF()
-    //
-    //    windowsMYSQLRDDDF.printSchema()
-    //
-    //    val windowsMYSQLup = new java.util.Properties()
-    //    windowsMYSQLup.put("user", "root")
-    //    windowsMYSQLup.put("password", "root")
-    //
-    //    windowsMYSQLRDDDF.write
-    //      .mode(SaveMode.Append)
-    //      .jdbc("jdbc:mysql://localhost:3306/test", "inserttest", windowsMYSQLup) //windows MYSQL
-    //
-    //    println("已完成sql插入 windowsMYSQL")
-    //    windowsMYSQLRDDDF.show()
-
-    // linuxMYSQL
-    val linuxSQLRDD: RDD[linuxMYSQL] = lines.map(line => {
+    //windowsMYSQL
+    val windowsSQLRDD: RDD[windowsMYSQL] = lines.map(line => {
       val arr: Array[String] = line.split(" ")
-      linuxMYSQL(
+      windowsMYSQL(
         arr(0).toInt
         , arr(1)
         , arr(2))
     })
     import spark.implicits._
-    val linuxMYSQLRDDDF: DataFrame = linuxSQLRDD.toDF()
+    val windowsMYSQLRDDDF: DataFrame = windowsSQLRDD.toDF()
 
-    linuxMYSQLRDDDF.printSchema()
+    windowsMYSQLRDDDF.printSchema()
 
+    val windowsMYSQLup = new java.util.Properties()
+    windowsMYSQLup.put("user", "root")
+    windowsMYSQLup.put("password", "root")
 
-    val linuxMYSQLup = new java.util.Properties()
-    linuxMYSQLup.put("user", "root")
-    linuxMYSQLup.put("password", "MMYqq123")
-
-    linuxMYSQLRDDDF.write
+    windowsMYSQLRDDDF.write
       .mode(SaveMode.Append)
-      .jdbc("jdbc:mysql://node3:3306/SouGouQueryAnalysis", "scala_JDBC_test", linuxMYSQLup) //windows MYSQL
-    println("已完成sql插入 linuxMYSQL")
-    linuxMYSQLRDDDF.show()
+      .jdbc("jdbc:mysql://localhost:3306/test", "inserttest", windowsMYSQLup) //windows MYSQL
+
+    println("已完成sql插入 windowsMYSQL")
+    windowsMYSQLRDDDF.show()
+
+    //    // linuxMYSQL
+    //    val linuxSQLRDD: RDD[linuxMYSQL] = lines.map(line => {
+    //      val arr: Array[String] = line.split(" ")
+    //      linuxMYSQL(
+    //        arr(0).toInt
+    //        , arr(1)
+    //        , arr(2))
+    //    })
+    //    import spark.implicits._
+    //    val linuxMYSQLRDDDF: DataFrame = linuxSQLRDD.toDF()
+    //
+    //    linuxMYSQLRDDDF.printSchema()
+    //
+    //
+    //    val linuxMYSQLup = new java.util.Properties()
+    //    linuxMYSQLup.put("user", "root")
+    //    linuxMYSQLup.put("password", "MMYqq123")
+    //
+    //    linuxMYSQLRDDDF.write
+    //      .mode(SaveMode.Append)
+    //      .jdbc("jdbc:mysql://node3:3306/SouGouQueryAnalysis", "scala_JDBC_test", linuxMYSQLup) //windows MYSQL
+    //    println("已完成sql插入 linuxMYSQL")
+    //    linuxMYSQLRDDDF.show()
 
 
     spark.stop()
